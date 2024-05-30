784ec40e9705909254800c6bc81ec15845fcf1d9
azriel123123 <mazrielilham4@gmail.com>
2024-05-30 11:29:55 +0700

    new

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,4 +16,12 @@ Route::get('/tak', [App\Http\Controllers\TaskController::class, 'index']);
 Route::middleware(['auth'])->group(function () {
     Route::resource('/task', TaskController::class);
     Route::patch('todos/{todo}/status', [TaskController::class, 'updateStatus']);
+});
+
+// Route Artisan Call
+Route::get('/artisan-call', function() {
+    Artisan::call('storage:link');
+    Artisan::call('route:clear');
+    Artisan::call('config:clear');
+    return 'success';
 });
